cfb814414e1de4290688f2e574e3460683849884
JINXZ <58348317+NotJINXZ@users.noreply.github.com>
2021-05-25 20:47:13 -0400

    -- RUN ME --

---
+++ script.lua
@@ -0,0 +1,11 @@
+--[[
+Made by Not JINXZ#9925
+Discord: dsc.gg/jinxz
+Github: Github.com/NotJINXZ
+
+Enjoy!
+]]--
+
+-- Run me in an executer --
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NotJINXZ/JINXZ-HUB/main/JINXZ-Hub.lua?token=AN5FGHLCYYNCXNITN6GIND3AVWNJW"))()
